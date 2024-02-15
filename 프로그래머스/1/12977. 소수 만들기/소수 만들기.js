4383244dea2d4e575689836db4760481ed89f6ab
tomeee11 <114478045+tomeee11@users.noreply.github.com>
2024-02-15 16:35:33 +0900

    [level 1] Title: 소수 만들기, Time: 1.92 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,23 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function solution(nums) {
+    var answer = 0;
+    for(let i=0; i<nums.length-2; i++){
+        for(let j=i+1; j<nums.length-1; j++){
+            for(let k=j+1; k<nums.length; k++){
+              let sum =nums[i]+nums[j]+nums[k]
+                if(isPrime(sum)){
+                    answer++
+                }
+            }
+        }
+    }
+    console.log('Hello Javascript')
+    return answer;
+}
