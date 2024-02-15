53cc4a070882c124dde81b943584f46e202b8115
tomeee11 <114478045+tomeee11@users.noreply.github.com>
2024-02-15 19:51:12 +0900

    [level 1] Title: 소수 찾기, Time: 115.59 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -25,12 +25,14 @@ function decimal(num){
     if (num <= 3) return true; // 2,3 은 홀수.
     if (num % 2 === 0 || num % 3 === 0) return false; // 2,3 배수는 소수가 아님.
     // i가 5일 경우 25<=num 반복을 돌지 않아 트루. 6,8,9,10... 2,3배수는 일단 false
-    // 5에서 6씩 증가하면 홀수.
-    for(let i=5; i*i<=num; i += 6){
+
+    for(let i=5; i*i<=num; i+=6){
+        //num의 숫자가 클 경우 반복문이 실행이 된다. 그러므로 반복횟수를 줄일 수 있다.
         if(num%i===0 || num%(i+2)===0){
             return false
         }
     }
+    //num의 숫자가 2,3의 배수가 아니고 num의 숫자가 25가 안될 경우 소수이다 
     return true
 }
 
