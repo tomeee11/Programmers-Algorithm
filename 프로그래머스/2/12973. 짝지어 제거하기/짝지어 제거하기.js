de2c0395073ec78abb21e08c96511073a9392008
tomeee11 <114478045+tomeee11@users.noreply.github.com>
2024-03-05 20:49:34 +0900

    [level 2] Title: 짝지어 제거하기, Time: 35.60 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,12 @@
+function solution(s){
+    var stack = []
+    for(let char of s){
+     if(stack.length===0 || stack[stack.length-1] !== char){
+         stack.push(char)
+     }else{
+         stack.pop()
+     }
+    }
+
+    return stack.length === 0 ? 1:0;
+}
