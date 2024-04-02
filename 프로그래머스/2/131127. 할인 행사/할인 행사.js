0f2a19cb76a1a10451b232cb75a7a8eb892bebf5
tomeee11 <114478045+tomeee11@users.noreply.github.com>
2024-04-02 12:15:55 +0900

    [level 2] Title: 할인 행사, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,26 +1,24 @@
 function solution(want, number, discount) {
-  let count = 0;
-  
-  // discount 배열을 순회하면서 10일간의 기간을 갖는 조각들을 만듦
-  for (let i = 0; i < discount.length - 9; i++) {
-    // 현재 10일간의 부분 배열을 구함
-    const slice = discount.slice(i, i + 10);
-	
-    // 원하는 제품의 원하는 개수와 일치하는지 확인
-    let flag = true;
-    for (let j = 0; j < want.length; j++) {
-      // 현재 제품에 대한 할인 횟수를 구함
-      const discounted  = slice.filter((item) => item === want[j]).length;
-      // 할인 횟수가 목표 수량과 일치하지 않으면 flag를 false로 설정하고 반복 중단
-      if (discounted  !== number[j]) {
-        flag = false;
-        break;
-      }
+    var count = 0;
+    /*
+    10일 연속으로 일치할 경우에 맞춰서 회원가입을 하려 합니다.
+    예를 들어, discount.length가 14라면 5번만 돌아야 합니다. (배열의 길이를 넘어가기 때문에)
+    */
+    for(let i = 0; i <= discount.length - 10; i++) { // 수정된 부분
+        // 슬라이스를 통해 discount[i]부터 discount[i+10]까지를 잘라서 배열을 형성합니다.
+        const slice = discount.slice(i, i + 10);
+        let flag = true;
+        for(let j = 0; j < want.length; j++) {
+        // slice에 filter를 적용하여 want[j]와 같은 원소의 개수를 구합니다.
+            const discounted = slice.filter((v) => v === want[j]).length;
+        // discounted와 number[j]를 비교하여 같지 않다면 flag를 false로 설정하고 반복문을                    종료합니다.
+            if(discounted !== number[j]) {
+                flag = false;
+                break;
+            }
+        }
+        // flag가 true인 경우에만 count를 증가시킵니다.
+        if(flag) count++;
     }
-    
-    // 원하는 제품들이 목표 수량만큼 존재하는 경우 count 증가
-    if (flag) count += 1;
-  }
-  
-  return count;
+    return count;
 }
